44ebb0d1f8f4ca8c36e0da97ff363124db704252
ekil <idgusqls0506@naver.com>
2022-05-14 22:29:45 +0900

    8주차 데이터 통신 이론 정리 및 실습 파일 업로드

--- practice_계산기/ekil/calculator/app.js
+++ practice_계산기/ekil/calculator/app.js
@@ -16,6 +16,7 @@ const $answer = document.querySelector('.screen');
 const $ac = document.querySelector('#ac');
 const $enter = document.querySelector('#enter');
 
+// const $number = document.querySelectorAll('#num');
 const $num0 = document.querySelector('#num0');
 const $num1 = document.querySelector('#num1');
 const $num2 = document.querySelector('#num2');
@@ -41,6 +42,8 @@ const $divide = document.querySelector('#divide');
 // 클릭 이벤트 발생시, 위의 동작을 실행하는데, 연산자 눌리기 전에 눌린 숫자라면 num1에, 연산자 눌린 후에 눌린 숫자라면 num2에 저장해야 할 것.
 
 // 반복문을 써서 아래 코드를 더 간단히 쓸 순 없나?
+// let clickedNumber = $number.target.textcontent
+// $number.addEventListener('click', onClickNum); -> not a function!
 $num0.addEventListener('click', onClickNum);
 $num1.addEventListener('click', onClickNum);
 $num2.addEventListener('click', onClickNum);
@@ -52,14 +55,24 @@ $num7.addEventListener('click', onClickNum);
 $num8.addEventListener('click', onClickNum);
 $num9.addEventListener('click', onClickNum);
 
+// for(i = 0 ; i < 10 ; 1++) {
+//     $numi.addEventListener('click', onClickNum);
+// }
+
+
 function onClickNum(e) {
     if (!operator) {
-        $answer.value = '';
-        num1 += e.target.textContent;
+        if ($answer.value === '0') {
+            $answer.value = '';
+            num1 += e.target.textContent;
+        } else {
+            num1 += e.target.textContent; // if문 중복 제거해보기
+        }
     } else {
         num2 += e.target.textContent;
     }
-    $answer.value += e.target.textContent;
+    num1 = $answer.value += e.target.textContent; // 연속 계산 위해서는 답을 저장해야 함
+    num2 = '' + e.target.textContent; // 연속 계산 위해서는 num2가 초기화되어야 함. 그런데 두 자리 이상의 숫자이려면 아예 없애서도 안 됨.
 }
 
 $plus.addEventListener('click', onClickOperator);
@@ -67,9 +80,10 @@ $minus.addEventListener('click', onClickOperator);
 $multiply.addEventListener('click', onClickOperator);
 $divide.addEventListener('click', onClickOperator);
 
-function onClickOperator(op) {
-    operator = op;
-    $answer.value += op;
+function onClickOperator(e) {
+    operator = e.target.textContent;
+    $answer.value += operator;
+    console.log(e.target.textContent);
 }
 
 // enter 누르면 연산 실행해 답을 $answer.value에 저장하기
@@ -95,4 +109,26 @@ function onClickEnter() {
             $answer.value = parseFloat(num1) / parseFloat(num2);
             break;
     }
+}
+
+// ac 기능 구현
+// ac 버튼 누르면 창의 문자열을 없애줌
+// num1과 num2 초기화도 필요할까?
+$ac.addEventListener('click', onClickAc);
+
+function onClickAc() {
+    $answer.value = '0'; // 0이 뜨는 건 잘 되는데, 그 뒤에 새로운 숫자를 눌렀을 때 0이 사라지지 않음. 왜?
+}
+
+// . 기능 구현
+$dot.addEventListener('click', onClickDot);
+
+function onClickDot(e) {
+    if (!operator) {
+        num1 += e.target.textContent;
+    } else {
+        num2 += e.target.textContent;
+    }
+    $answer.value += e.target.textContent;
+    console.log(num1, operator, num2, $answer.value); // num2가 이상하다. num1도 이상하다. 뺄셈도 이상하다.
 }
